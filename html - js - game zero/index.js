501f16496e5d5612c3776352573a5a463958b3c9
Xostron <aat9044107480@gmail.com>
2023-02-07 13:23:37 +0300

    отписка подписка на событие ячейки

--- html - js - game zero/index.js
+++ html - js - game zero/index.js
@@ -2,44 +2,58 @@
 const cells = document.querySelectorAll(".cell");
 // комбинации
 const combination = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8]
-]
-// подписка на click для всех ячеек
-cells.forEach((cell, idx) => {
-  cell.addEventListener("click", () => handlerCell(cell, idx));
-});
-
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+// флаг winner
+let winner = "";
+// счетчик ходов
 let count = 0;
 
-function handlerCell(cell, idx) {
+// объект для подписки и отписки события click ячейки
+const objectsEvents = [];
+cells.forEach((cell, idx) => {
+  objectsEvents.push({
+    handleEvent: handlerCell,
+    cell,
+    idx,
+  });
+});
+
+// подписка на click для всех ячеек
+cells.forEach((cell, idx) => {
+  cell.addEventListener("click", objectsEvents[idx]);
+});
+
+// обработка события click ячейки
+function handlerCell() {
   // занята ли ячейка
-  if (cell.classList.length > 2) {
+  if (this.cell.classList.length > 2) {
     return;
   }
-
-  // крестик - четные или нолик - нечетные
+  // Очередность хода - крестик:четные или нолик:нечетные
   // крестик
   if (count % 2 === 0) {
-    cell.classList.remove("cell__0");
-    cell.classList.add("cell__x");
+    this.cell.classList.remove("cell__0");
+    this.cell.classList.add("cell__x");
   }
   // нолик
   else {
-    cell.classList.remove("cell__x");
-    cell.classList.add("cell__0");
+    this.cell.classList.remove("cell__x");
+    this.cell.classList.add("cell__0");
   }
   count++;
-  console.log(count);
+  console.log("handlerCell", count, this);
   // проверка выйгрыша хода
   win();
-  end()
+  // проверка endgame
+  end();
 }
 
 // проверка выйгрыша хода
@@ -47,50 +61,46 @@ function win() {
   if (count < 5) {
     return;
   }
-//   console.log("win?");
   for (const combi of combination) {
-    // console.log(combi[0], combi[1], combi[2])
-    // console.log(cells[combi[0]].className,cells[combi[1]].className,cells[combi[2]].className)
-    if (cells[combi[0]].className === cells[combi[1]].className &&
-        cells[combi[0]].className === cells[combi[2]].className){
-            if (cells[combi[0]].className === 'cell cell__empty cell__0'){
-                alert("Нолики is WIN")
-            }
-            else if (cells[combi[0]].className === 'cell cell__empty cell__x'){
-                alert("Крестики is WIN")
-            }
-            
-            // cells.forEach((cell,idx)=>{cell.removeEventListener('click', ()=>handlerCell(cell,idx))})
-            
-        }
+    if (
+      cells[combi[0]].className === cells[combi[1]].className &&
+      cells[combi[0]].className === cells[combi[2]].className
+    ) {
+      if (cells[combi[0]].className === "cell cell__empty cell__0") {
+        winner = "нолики";
+        console.log("Нолики is WIN");
+      } else if (cells[combi[0]].className === "cell cell__empty cell__x") {
+        console.log("Крестики is WIN");
+        winner = "крестики";
+      }
+      // не отписывается событие от кнопки? не отписывалась потому что
+      // каждый раз указывался объект не имеющий ссылки
+      console.log("delete callback");
+      cells.forEach((cell, idx) => {
+        cell.removeEventListener("click", objectsEvents[idx]);
+      });
+    }
   }
-// combination.forEach((combi =>{
-//     console.log(combi)
-// console.log(cells[combi[0]])
-// }))
-
 }
 
 // конец игры
-function end(type=''){
-    if (count===9) {
-        console.log('конец игры - ничья')
-    }
-    if (type==='x') {
-        console.log('конец игры - победили крестики')
-    }
-    if (type === '0'){
-        console.log('конец игры - победили нолики')
-    }
+function end(type = "") {
+  if (count === 9) {
+    console.log("конец игры - ничья");
+  }
 }
 
-
-// перезагрузить игру 
-function reload(){
-    console.log('reload')
-    cells.forEach(cell=>{
-cell.classList.remove('cell__x')
-cell.classList.remove('cell__0')
-count = 0
-    })
+// кнопка перезагрузить игру
+function handlerReload() {
+  console.log("reload");
+  cells.forEach((cell) => {
+    cell.classList.remove("cell__x");
+    cell.classList.remove("cell__0");
+    count = 0;
+    winner = "";
+    // подписка на click для всех ячеек
+    cells.forEach((cell, idx) => {
+      cell.addEventListener("click", objectsEvents[idx]);
+    });
+  });
 }
