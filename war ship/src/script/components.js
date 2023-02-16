887de11c9f16401a6b67c381c6092cf9d0845d02
xos <aat9044107480@gmail.com>
2023-02-16 17:31:22 +0300

    events d&d

---
+++ war ship/src/script/components.js
@@ -0,0 +1,135 @@
+// Global
+// input field
+let player1,
+  player2 = "";
+// функция отображения компонетов
+function render(template, where, wrapper = true) {
+  if (wrapper) {
+    const container = document.createElement("div");
+    container.innerHTML = template;
+    where.append(container);
+    return container;
+  } else {
+    where.innerHTML = template;
+    return where;
+  }
+}
+// отображение фантомной копии
+const DragEl = (tool, idx, where) => {
+  const { size } = tool;
+  const imgShip = {
+    front: `<img class="cell" src="./src/source/icon/1xh.svg"/>`,
+    middle: `<img class="cell" src="./src/source/icon/0x.svg"/>`,
+    back: `<img class="cell" src="./src/source/icon/1xh.svg"/>`,
+  };
+  let container = document.createElement("div");
+  container.classList.add("drag-el");
+  container.id = `drag${idx}`;
+  for (let i = 0; i < size; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.dataset.state = "ship";
+    i === 0
+      ? (cell.innerHTML += imgShip.front)
+      : (cell.innerHTML += imgShip.middle);
+    container.append(cell);
+  }
+  where.append(container);
+};
+// окно логина
+const Login = (element, display = true) => {
+  let template = `
+  <span class="login-title">Морской бой</span>
+  <span class="login-subtitle"> 1 на 1</span>
+  <div class="login-input">
+  <input class="input" type="text" id="in1" placeholder="Игрок 1"/>
+  </div>
+  <div class="login-input">
+  <input class="input" type="text" id="in2" placeholder="Игрок 2"/>
+  </div>
+  <button class="btn btn-text">Начать игру</button>
+  <div class="blur"></div>
+  `;
+  display ? render(template, element, false) : render("", element, false);
+  if (display) {
+    const hndlStart = element.querySelector(".btn-text");
+    const in1 = element.querySelector("#in1");
+    const in2 = element.querySelector("#in2");
+    // ====подписка на события====
+    hndlStart.addEventListener("click", () => {
+      hndlLoginStart(in1, in2);
+    });
+  }
+};
+// handlers Login
+function hndlLoginStart(in1, in2) {
+  player1 = in1.value ? in1.value : "Игрок 1";
+  player2 = in2.value ? in2.value : "Игрок 2";
+  Login(login, false);
+  Field(field, arrField);
+  Toolbar(header, tools);
+}
+// игровое поле
+const Field = (element, arr = [[0]], render = true) => {
+  for (let i = 0; i < arr.length; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < arr[0].length; j++) {
+      let cell = document.createElement("div");
+      cell.dataset.state = "cell";
+      cell.classList.add("cell");
+      row.append(cell);
+    }
+    element.append(row);
+  }
+  element.classList.add("bg");
+  if (render) {
+  }
+};
+// панель инструментов - корабли
+const Toolbar = (where, elements) => {
+  let template = `
+  <div class="header-inner">
+  <span>Расположите корабли на поле</span>
+  <div class="toolbar"></div>
+  </div>`;
+  render(template, where, false);
+  const toolbar = document.querySelector(".toolbar");
+  elements.map((tool, idx) => Tool(toolbar, tool, idx));
+};
+// инструменты
+let selected={}
+const Tool = (where, tool, idx) => {
+  const { img, sum,size } = tool;
+  const template = `
+<div draggable="true" id="btn${idx}" class="tool">
+<img class="img-ship" src="${img}" alt=""/>
+<span class="sum">${sum}</span>
+</div>`;
+  const container = render(template, where);
+  const item = container.querySelector(`#btn${idx}`);
+  // ====подписка на события====
+  item.addEventListener("click", () => {
+    tool.sub();
+  });
+
+  item.addEventListener("dragstart", (ev) => {
+    // выбор фантомной копии - находится за пределами экрана
+    const el = document.querySelector(`#drag${idx}`);
+    // отображение фантомной копии
+    ev.dataTransfer.setDragImage(el, 15, 15);
+    // устанавливаем данные перетаскивания
+    let dt = ev.dataTransfer
+    dt.setData("ship", JSON.stringify(tool))
+    // global - выбранный корабль
+    selected = JSON.parse(dt.getData("ship"))
+  });
+
+  // item.addEventListener("dragend", (ev) => {
+  //   console.log("dragend=", ev);
+  // });
+
+  // item.addEventListener("drop",(ev)=>{
+  //   console.log("tool drop",ev)
+  // })
+};
--- war ship/src/script/index.js
+++ war ship/src/script/index.js
@@ -1,8 +1,8 @@
 const login = document.querySelector(".login");
 const field = document.querySelector(".field");
 const header = document.querySelector(".header");
-let player1,
-  player2 = "";
+const phantom = document.querySelector(".phantom");
+
 const SIZE = 10;
 const state = {
   cell: 0,
@@ -20,125 +20,43 @@ const tools = [
 ];
 let arrField = new Array(SIZE).fill(new Array(SIZE).fill(state.cell));
 
-function renderDragEl (tool,idx){
-  const {size} = tool
-  let dragEl = document.createElement("div");
-    dragEl.classList.add('drag-el')
-    dragEl.id=`drag${idx}`
-    let imgF = `
-    <img
-    class="cell"
-    src="./src/source/icon/1xh.svg"
-    />
-  `;
-    let imgB = `
-    <img
-    class="cell"
-    src="./src/source/icon/0x.svg"
-    />
-  `;
-    for (let i = 0; i < size; i++) {
-      let cell = document.createElement('div')
-    cell.classList.add("cell");
-      i === 0 ? (cell.innerHTML += imgF) : (cell.innerHTML += imgB);
-      dragEl.append(cell)
-    }
-    field.append(dragEl);
-}
-tools.map((val,idx)=>renderDragEl(val,idx))
-// компоненты
-// окно логина
-const Login = (element, render = true) => {
-  let template = `
-<span class="login-title">Морской бой</span>
-<span class="login-subtitle"> 1 на 1</span>
-<div class="login-input">
-<input class="input" type="text" id="in1" placeholder="Игрок 1"/>
-</div>
-<div class="login-input">
-<input class="input" type="text" id="in2" placeholder="Игрок 2"/>
-</div>
-<button class="btn btn-text">Начать игру</button>
-<div class="blur"></div>
-`;
-  element.innerHTML = render ? template : "";
-  if (render) {
-    const hndlStart = element.querySelector(".btn-text");
-    const in1 = element.querySelector("#in1");
-    const in2 = element.querySelector("#in2");
-    hndlStart.addEventListener("click", () => {
-      player1 = in1.value ? in1.value : "Игрок 1";
-      player2 = in2.value ? in2.value : "Игрок 2";
-      Login(login, false);
-      Field(field, arrField);
-      Toolbar(header, tools);
+// подписка на события
+field.addEventListener("dragenter", (ev) => {
+  console.log("field dragenter = ", selected);
+
+  const cells = field.querySelectorAll(".cell");
+
+  const absIdx = Array.from(cells).indexOf(ev.target) + 1;
+  let int = Math.trunc((Array.from(cells).indexOf(ev.target) + 1) / 10)
+  const rltIdx =
+    absIdx - (int* 10);
+  const sizeRow = 10;
+  // очистка ячеек
+  cells.forEach((cell, idx) => {
+    cell.dataset.state = "cell";
+  });
+  // подкрашивание выбранных ячеек
+  if (rltIdx + selected.size - 1 <= sizeRow) {
+    let arr = Array.from(cells).slice(absIdx - 1, absIdx + selected.size - 1);
+    console.log("slice", arr, absIdx, rltIdx, (rltIdx + selected.size - 1 ));
+    arr.forEach((val) => {
+      val.dataset.state = "ship";
     });
   }
-};
-// игровое поле
-const Field = (element, arr = [[0]], render = true) => {
-  for (let i = 0; i < arr.length; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < arr[0].length; j++) {
-      let cell = document.createElement("div");
-      cell.dataset.state = "cell";
-      cell.classList.add("cell");
-      row.append(cell);
-    }
-    element.append(row);
+});
+
+field.addEventListener("dragover", (ev) => {
+  // разрешаем сбрасывать элементы в ячейки
+  if (ev.target !== ev.currentTarget) {
+    ev.preventDefault();
   }
-  element.classList.add("bg");
-  if (render) {
-  }
-};
-// панель инструментов - корабли
-const Toolbar = (where, elements) => {
-  let template = `
-<div class="header-inner">
-<span>Расположите корабли на поле</span>
-<div class="toolbar"></div>
-  `;
-  where.innerHTML = template;
-  const toolbar = document.querySelector(".toolbar");
-  elements.map((tool, idx) => Tool(toolbar, tool, idx));
-};
+});
 
-const Tool = (where, tool, idx) => {
-  const { img, sum,size } = tool;
-  const template = `
-<div draggable="true" id="btn${idx}" class="tool">
-<img
-  class="img-ship"
-  src="${img}"
-  alt=""
-/>
-<span class="sum">${sum}</span>
-</div>
-`;
-  const fragment = document.createElement("div");
-  fragment.innerHTML = template;
-  const item = fragment.querySelector(`#btn${idx}`);
-  const icon = fragment.querySelector(".img-ship");
-  // icon.classList.add('selected-icon')
-  where.append(fragment);
-  // ====подписка на события====
-  item.addEventListener("click", () => {
-    tool.sub();
-  });
-
-  item.addEventListener("dragstart", (ev) => {
-    
-    const el = document.querySelector(`#drag${idx}`)
-
-    // ev.dataTransfer.dropEffect = "move";
-    ev.dataTransfer.effectAllowed = "move";
-    ev.dataTransfer.setDragImage(el, 15, 15);
-  });
-
-  item.addEventListener("dragend", (ev) => {});
-};
-
+field.addEventListener("drop", (ev) => {
+  let a = JSON.parse(ev.dataTransfer.getData("ship"));
+  console.log("field drop = ", ev.dataTransfer, a);
+});
 
 // вызов программы
 Login(login);
+tools.map((val, idx) => DragEl(val, idx, phantom));
