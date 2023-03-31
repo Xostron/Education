81f590b9538d45af6dba2bebc7012f8a9f3e73de
xostron <aat9044107480@gmail.com>
2023-03-31 09:25:40 +0300

    test

--- 0_node_express/xos0/main.js
+++ 0_node_express/xos0/main.js
@@ -1,39 +1,39 @@
-const http = require("http")
-const fs = require("fs")
-const port = process.env.PORT || 3000
+const http = require("http");
+const fs = require("fs");
+const port = process.env.PORT || 3000;
 
 function serveStaticFile(res, path, contentType, responseCode = 200) {
   fs.readFile(__dirname + path, (err, data) => {
     if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" })
-      return res.end("500 - Внутрення ошибка")
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      return res.end("500 - Внутрення ошибка");
     }
-    res.writeHead(responseCode, { "Content-Type": contentType })
-    res.end(data)
-  })
+    res.writeHead(responseCode, { "Content-Type": contentType });
+    res.end(data);
+  });
 }
 
 const server = http.createServer((req, res) => {
-  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase()
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
   switch (path) {
     case "":
-      serveStaticFile(res, "/public/main.html", "text/html")
-      break
+      serveStaticFile(res, "/public/main.html", "text/html");
+      break;
     case "/q":
-      serveStaticFile(res, "/public/play.html", "text/html")
-      break
+      serveStaticFile(res, "/public/play.html", "text/html");
+      break;
     case "/img/logo.png":
-      serveStaticFile(res, "/public/img/logo.png", "image/png")
-      break
+      serveStaticFile(res, "/public/img/logo.png", "image/png");
+      break;
     default:
-      serveStaticFile(res, "/public/404.html", "text/html")
-      break
+      serveStaticFile(res, "/public/404.html", "text/html");
+      break;
   }
-})
+});
 
 server.listen(port, () =>
   console.log(
     `Server running success on PORT = ${port}` +
       "\npush Ctrl+C for exit program"
   )
-)
+);
