ec24324188006bb4af3567279b384649c13d8c29
xostron <aat9044107480@gmail.com>
2023-04-06 15:19:15 +0300

    немного о промисах и цепочке промисов

--- 0_node_express/xos2/server2/utils/testMongo/index.js
+++ 0_node_express/xos2/server2/utils/testMongo/index.js
@@ -12,9 +12,10 @@ db.on("connect", () => console.log("Связь с MongoDB установлена
 // создание документа в коллекции  alpha***************************
 // db.alpha.save({created: 'just now'})
 // db.alpha.save({
-//     time:'10:00',
+//     time1:'10:00',
 //     delta:'00:30',
-//     author:'xostron'
+//     author:'beta',
+//     distance:''
 // })
 
 // find everything***********************************
@@ -53,7 +54,7 @@ db.on("connect", () => console.log("Связь с MongoDB установлена
 //     console.log(doc)
 // })
 
-/* Задание 1 ***************************
+/* Задание 1 *****************************************
 Doc{
 _id:642e6bf50ad7820768eb0687
 time:"10:00"
@@ -81,7 +82,7 @@ function() - вызывается когда обновление выполне
 //   }
 // );
 
-/* ***************************
+/* **************************************************
 найти и удалить документы которые не соответствуют формату Doc из задания 1
 remove({1},{2}) 
 {1} - поисковый фильтр
@@ -100,3 +101,94 @@ remove({1},{2})
 //   }
 // });
 
+/* **************************************************
+Использование Promise
+
+Имеется функция comLoken, возвращающая Promise, далее экзекъютор промиса выполняется:
+инициализируется поиск (курсор cur) по запросу q ($ne - если поле author!== 'xostron') 
+cur с попиской на события err - событие ошибка, end - событие завершение операции, 
+data - событие вызывается при каждом нахождении документа.
+
+в данных обработчиках событий выполняется работа с найденный документом
+ (например нашелся документ, и по нему мы можем сделать чтонибудь еще) 
+ и обязательно вызываем функцию resolve 
+ (для того чтобы сказать об успешности операции) или reject (об ошибке). 
+ resolve,reject - принимает в себя аргумент(что угодно *)
+
+ Как происходит вызов функции comLoken:
+
+ comLoken возвращает промис, для обработки промиса, навешиваются обработчики then()
+ -успешное выполнение (resolve), catch - ошибка (reject) - Это внешний код, 
+ он вызывается автоматически в зависимости от того как логика отработала в экзекъюторе 
+ промиса c вызовом resolve(*Y) или reject(*N).
+
+ *Y, *N - это опциональные аргументы котороые мы можем передать в resolve/reject
+ когда внешний код then/catch ловят resolve/reject - эти аргументы передаются к ним
+ 
+ then((*Y)=>{})
+ catch((*N)=>{})
+
+немного о цепочке промисов
+
+.then((*Y)=>{
+    console.log(*Y)
+    return X
+})
+.then((X)=>{
+
+})
+если then() возвращает что-то, оно автоматически является промисом, и обрабатывается 
+также через then (следующий по очереди), так образуется цепочка промисов.
+*/
+
+function comLoken() {
+    return new Promise((resolve, reject) => {
+        const q = { author: { $ne: 'xostron' } }
+        const cur = db.alpha.find(q)
+
+        let count = 0
+        let end = false
+
+        cur.on('error', err => reject(err))
+        cur.on('end', _ => {
+            end = true
+            if (count) return
+            resolve(true)
+        })
+        cur.on('data', doc => {
+            // ++count
+            // const agentId = doc.agentId
+            // doc.owner = {
+            //     type: 'agent',
+            //     id: agentId
+            // }
+            // db.promo.updateOne({ _id: doc._id }, { $set: doc })
+            // if (--count) return
+            // if (!end) return
+            console.log('doc = ', doc)
+            resolve(true)
+        })
+    })
+}
+
+// function update(q, un, m) {
+//     return new Promise((resolve, reject) => {
+//         db.promo.update(q, un, m, (err, docs) => {
+//             if (err) return reject(err)
+//             resolve(docs)
+//         })
+//     })
+// }
+
+comLoken()
+    // .then(_ => {
+    //     // const q = { agentId: { $ne: null } }
+    //     // const un = { $unset: { agentId: 1 } }
+    //     // const m = { multi: true }
+    //     // return update(q, un, m)
+    // })
+    .then(_ => {
+        db.close()    
+        console.log('БД обновлена')
+    })
+    .catch(err => console.log(err))
