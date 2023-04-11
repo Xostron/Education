4d105d26ea27cc66fef911d3c8aa729d5f9ddba4
xostron <aat9044107480@gmail.com>
2023-04-11 15:52:52 +0300

    node: convert to exe

---
+++ 0_node_express/xos3/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const pug = require("pug");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// определение папки для статических файлов
+app.use(express.static(__dirname + "/public"));
+// объявление движка pug
+app.set("views", path.join(__dirname, "public/views"));
+app.set("view engine", "pug");
+
+// маршруты
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/reg", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("home");
+});
+// redirect - при не правильном маршруте, перенаправляет на домашнюю страницу
+app.use("*", (req, res) => {
+  res.redirect('/')
+});
+
+app.listen(port, () =>
+  console.log(`Сервер запущен по адресу http://localhost:${port}`)
+);
---
+++ 0_node_express/xos3/public/script/home.js
@@ -0,0 +1,37 @@
+console.log('test xostron3')
+const a = {
+    name:'xos',
+    city:'loken',
+    phone:'904410'
+}
+
+function match(obj={}, keys=[]){
+    let dif = []
+    const kObj = Object.keys(obj)
+    keys.forEach((key,idx)=>{
+        if (!kObj.includes(key)) {
+            dif.push(key)
+        }
+    })
+    return dif
+}
+
+function match2(obj={}, keys=[]){
+    let dif = []
+    keys.forEach((key,idx)=>{
+        if (!a.hasOwnProperty(key)) {
+            dif.push(key)
+        }
+    })
+    return dif
+}
+
+console.log(match(a, ['age', 'address', 'name', 'city','email']))
+
+console.log(match2(a,['age', 'address', 'name', 'city','email']))
+
+console.log(this)
+
+let q = [0,3,2,5,7,4,8,1,19,17,12,15,29,21]
+
+console.log(q.sort((a,b)=>{return a-b}))
