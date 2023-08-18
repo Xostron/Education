006ac1ca1fd883c8835ba1ffe47c5cd9085c06f9
xostron <aat9044107480@gmail.com>
2023-08-18 09:32:20 +0300

    r-router-dom+code splitting(асин подгрузка бандла)

--- 4_react_education/1_module/4_webpack/config/build/buildDevServer.ts
+++ 4_react_education/1_module/4_webpack/config/build/buildDevServer.ts
@@ -1,11 +1,12 @@
 import { BuildOptions } from "./types/config";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-
 // webpack-dev-server - это простой веб-сервер, который автоматически перезапускает сборку проекта
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
 	return {
 		port: options.port,
-        open:true
+		open: true,
+		// доступ к dev-server с произвольного url
+		historyApiFallback: true,
 	};
 }
--- 4_react_education/1_module/4_webpack/src/App.tsx
+++ 4_react_education/1_module/4_webpack/src/App.tsx
@@ -1,11 +1,23 @@
-import { Counter } from "./components/Counter";
+import { Routes, Route } from "react-router-dom";
 import "./index.scss";
+import MainPage from "./pages/main";
+import AboutPage from "./pages/about";
+import { Link } from "react-router-dom";
+import { Suspense } from "react";
+import { MainPageAs } from './pages/main/index.async';
+import { AboutPageAs } from './pages/about/index.async';
 
 export default function App() {
 	return (
 		<div className='app'>
-			<p>Webpack React+Typescript</p>
-			<Counter />
+			<Link to={"/"}>Main</Link>
+			<Link to={"/about"}>About</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={"/"} element={<MainPageAs />} />
+					<Route path={"/about"} element={<AboutPageAs />} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 }
