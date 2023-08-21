980b3284d4fad933708fb3fb4733d4ceee222110
xostron <aat9044107480@gmail.com>
2023-08-21 09:07:30 +0300

    git ignore test

--- 4_react_education/1_module/4_webpack/config/build/buildDevServer.ts
+++ 4_react_education/1_module/4_webpack/config/build/buildDevServer.ts
@@ -1,7 +1,8 @@
 import { BuildOptions } from "./types/config";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-// webpack-dev-server - это простой веб-сервер, который автоматически перезапускает сборку проекта
+// webpack-dev-server - это простой веб-сервер,
+// который автоматически перезапускает сборку проекта
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
 	return {
 		port: options.port,
