c09f1ab37970f6925571978d7fe7522d6f760720
xostron <aat9044107480@gmail.com>
2023-08-29 12:21:02 +0300

    add first test

--- 4_react_education/2_module/1_UI/src/shared/lib/classname/classname.test.ts
+++ 4_react_education/2_module/1_UI/src/shared/lib/classname/classname.test.ts
@@ -1,16 +1,21 @@
-import { classname } from './classname'
+import { classname } from "./classname"
 
+// Unit test
 describe("classnames", () => {
-    test('Проверка основного стиля',()=>{
-        expect(classname('main',{},[])).toBe('main')
-    })
-    test('Проверка основного стиля + модификатор {}',()=>{
-        expect(classname('main',{'active':true, 'abs':false},[])).toBe('main active')
-    })
-    test('Проверка основного стиля + дополнительного []',()=>{
-        expect(classname('main',{},['out'])).toBe('main out')
-    })
-    test('Проверка полного набора стилей',()=>{
-        expect(classname('main',{'active':true},['out'])).toBe('main out active')
-    })
+	test("Проверка основного стиля", () => {
+		expect(classname("main", {}, [])).toBe("main")
+	})
+	test("Проверка основного стиля + модификатор {}", () => {
+		expect(classname("main", { active: true, dark: undefined }, [])).toBe(
+			"main active"
+		)
+	})
+	test("Проверка основного стиля + дополнительного []", () => {
+		expect(classname("main", {}, ["out"])).toBe("main out")
+	})
+	test("Проверка полного набора стилей", () => {
+		expect(classname("main", { active: true }, ["out"])).toBe(
+			"main out active"
+		)
+	})
 })
