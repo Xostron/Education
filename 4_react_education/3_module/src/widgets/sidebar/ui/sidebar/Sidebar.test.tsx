0efe32b81ffd045019556006591e80ddc758aa8a
xostron <aat9044107480@gmail.com>
2023-09-13 11:33:25 +0300

    test sidebar

--- 4_react_education/3_module/src/widgets/sidebar/ui/sidebar/Sidebar.test.tsx
+++ 4_react_education/3_module/src/widgets/sidebar/ui/sidebar/Sidebar.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react' 
+import {fireEvent, render, screen} from '@testing-library/react' 
 import { withTranslation } from 'react-i18next'
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/fn'
 import { Sidebar } from 'widgets/sidebar/ui/sidebar'
@@ -9,8 +9,14 @@ import { Sidebar } from 'widgets/sidebar/ui/sidebar'
 describe("Sidebar", () => {
 	test("Проверка Sidebar render", () => {
 		renderWithTranslation(<Sidebar/>)
-        expect(screen.getByTestId('test2')).toBeInTheDocument()
+        expect(screen.getByTestId('@sidebar')).toBeInTheDocument()
 	})
-
+	test("Переключение скрыть/показать Sidebar", () => {
+		renderWithTranslation(<Sidebar/>)
+		const toggleBtn = screen.getByTestId('@sidebar-toggle')
+        expect(screen.getByTestId('@sidebar')).toBeInTheDocument()
+		fireEvent.click(toggleBtn)
+		expect(screen.getByTestId('@sidebar')).toHaveClass('collapsed')
+	})
 }) 
 
--- 4_react_education/3_module/src/widgets/sidebar/ui/sidebar/index.tsx
+++ 4_react_education/3_module/src/widgets/sidebar/ui/sidebar/index.tsx
@@ -6,15 +6,23 @@ import { LangSwitcher } from "shared/ui/lang_switcher"
 import { useTranslation } from "react-i18next"
 import { Button, ThemeButton } from "shared/ui/btn"
 
-export const Sidebar = () => {
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
 	const [collapse, setCollapse] = useState(false)
 	const { t } = useTranslation()
 	function hndlCollapse() {
 		setCollapse((prev) => !prev)
 	}
 	return (
-		<div className={classname(cls.main, { [cls.collapsed]: collapse }, [])} data-testid="test2">
+		<div
+			className={classname(cls.main, { [cls.collapsed]: collapse }, [className])}
+			data-testid='@sidebar'
+		>
 			<Button
+				data-testid='@sidebar-toggle'
 				className={cls.toggle}
 				theme={ThemeButton.CLEAR}
 				onClick={hndlCollapse}
@@ -22,7 +30,13 @@ export const Sidebar = () => {
 				{t("...Скрыть")}
 			</Button>
 
-			<div className={classname(cls.switchers, { [cls.switchers_collapsed]: collapse }, [])}>
+			<div
+				className={classname(
+					cls.switchers,
+					{ [cls.switchers_collapsed]: collapse },
+					[]
+				)}
+			>
 				<ThemeSwitcher />
 				<LangSwitcher className={cls.lang} />
 			</div>
