c8142346d336fedd2c383d2eaa96f62818755494
xostron <aat9044107480@gmail.com>
2023-09-14 08:44:39 +0300

    react-native: scrollView, SegmentedBttton

--- 5_react_native/xos1/src/screen/home/ui/index.js
+++ 5_react_native/xos1/src/screen/home/ui/index.js
@@ -1,13 +1,105 @@
-import { View, Text, Button } from "react-native"
+import { useState } from "react"
+import { View, Text, Button, ScrollView } from "react-native"
 import { cls } from "./style.js"
+import { SegmentedButtons } from "react-native-paper"
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export const Home = ({navigation}) => {
+export const Home = ({ navigation }) => {
+	const [value, setValue] = useState("")
+	console.log('value', value)
 	return (
 		<View style={cls.container}>
+			<ScrollView style={cls.scrollView} horizontal={true}>
+				<SegmentedButtons
+					value={value}
+					onValueChange={setValue}
+					style={cls.segment}
+					buttons={[
+						{
+							value: "walk",
+							label: "Walking",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "train",
+							label: "Transit",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "drive",
+							label: "Driving",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "1",
+							label: "1",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "2",
+							label: "2",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "3",
+							label: "3",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "4",
+							label: "4",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "5",
+							label: "5",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "6",
+							label: "6",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+						{
+							value: "7",
+							label: "7",
+							style: cls.btn,
+							labelStyle: cls.lbl,
+							showSelectedCheck: true,
+						},
+					]}
+				/>
+			</ScrollView>
 			<Text style={cls.text}>HOME SCREEN</Text>
-            <Button onPress={()=>navigation.navigate('Catalog', {data:'hello'})} title='Catalog+'/>
+			<Button
+				onPress={() =>
+					navigation.navigate("Catalog", { data: "hello" })
+				}
+				title='Catalog+'
+			/>
 		</View>
 	)
+}
+
+function wrapper() {
+	return <View></View>
 }
--- 5_react_native/xos1/src/screen/home/ui/style.js
+++ 5_react_native/xos1/src/screen/home/ui/style.js
@@ -1,16 +1,43 @@
 import { StyleSheet } from "react-native"
 
-
 const cls = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: "#15f",
+		// flex: 1,
+		backgroundColor: "#eee",
 		paddingHorizontal: 10,
-        alignItems:'center',
-        justifyContent:'center',
+		// alignItems:'center',
+		// justifyContent:'center',
 	},
 	text: {
+		color: "#000",
+	},
+	segment: {
+		paddingVertical: 10,
+		borderTop: 2,
+		borderColor: "#000",
+	},
+	btn: {
+		marginHorizontal: 15,
+		backgroundColor: "#ffbae8",
+		borderRadius: 16,
+		borderColor: "transparent",
+		borderTopRightRadius:16,
+		borderBottomRightRadius:16,
+		borderTopLeftRadius:16,
+		borderBottomLeftRadius:16,
+	},
+	lbl: {
 		color: "#fff",
+	},
+	btn_empty: {
+		opacity: 0,
+		pointerEvents: "none",
+		maxWidth:0,
+		minWidth:0
+	},
+	scrollView: {
+		// height:
+		backgroundColor:'#fff'
 	},
 })
 
