5193a5e43a6f3ef86697e998997a99d0cd5146a1
Xostron <aat9044107480@gmail.com>
2023-09-24 23:26:22 +0300

    event loop

--- 2_js_sandbox/JS/index3.js
+++ 2_js_sandbox/JS/index3.js
@@ -1,4 +1,42 @@
 console.log("Index 3");
+// event loop - микротаски и макротаски
+console.log("start");
+setTimeout(() => console.log(2), 0);
+Promise.resolve().then((_) => {
+	setTimeout(() => {
+		console.log(0);
+	}, 0);
+});
+Promise.resolve().then((_) => console.log("promise1"));
+console.log("mid");
+const p = new Promise((resolve) => {
+	console.log("p");
+	resolve();
+});
+setTimeout(() => console.log(3), 0);
+Promise.resolve().then((_) => console.log("promise2"));
+p.then((_) => console.log("promise3"));
+console.log("end");
+/* 
+ответ:
+start
+mid
+p
+end
+promise1
+promise2
+promise3
+2
+3
+0
+выполняется start, заносится в макротаски (2), заносится в микротаски (setTimeout(0)), заносится в микротаски (setTimeout(0), promise1)
+выполняется mid, выполняется p (сохранение в переменную р), заносится в макротаски (2, 3), заносится в микротаски (setTimeout(0), promise1, promise 2, promise3)
+выполняется end, проверка и выполнение всех микротасков: сначала setTimeout(0), который заносится в макротаски (2, 3, 0)
+выполнение оставшиъ микротасков promise1, promise 2, promise3, 
+начинается выполнение макротаска (setTimeout2), проверка микротасков - она пуста, выполнение микротаска (setTimeout3), проверка микротасков - она пуста
+начинается выполнение макротаска (setTimeout0), проверка микротасков - она пуста, макротаски - пусты - Ожтдание макрозадач (события мыши и т.д.)
+*/
+
 
 // async function main(){
 //     console.log('a')
@@ -49,20 +87,20 @@ console.log("Index 3");
 //     const r = await get()
 //     console.log('b = ',r)
 // }
-function get(i) {
-	return new Promise((resolve, reject) => {
-		setTimeout(()=>resolve(i),1000)
-	});
-}
+// function get(i) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(()=>resolve(i),1000)
+// 	});
+// }
 // a()
 // b()
 // get().catch(err=>console.log('@',err))
 
 // let promise = Promise.resolve();
-async function d() {
-	for (let i = 0; i < 10; i++) {
-		const r = await get(i);
-		console.log(r);
-	}
-}
-d()
+// async function d() {
+// 	for (let i = 0; i < 10; i++) {
+// 		const r = await get(i);
+// 		console.log(r);
+// 	}
+// }
+// d()
