20d4c03c260f5c1068f3b76dfe1217c45d732451
Xostron <aat9044107480@gmail.com>
2023-12-18 21:46:44 +0300

    isNan

--- 2_js_sandbox/JS/fetch.js
+++ 2_js_sandbox/JS/fetch.js
@@ -32,31 +32,62 @@
 // p.then((_) => console.log('promise3'))
 // console.log('end')
 
-const b = {
-	a: 1,
-	b: {
-		a: 10,
-		b: 100,
-		c: 1000,
-	},
+// const b = {
+// 	a: 1,
+// 	b: {
+// 		a: 10,
+// 		b: 100,
+// 		c: 1000,
+// 	},
+// };
+
+// const c = JSON.parse(JSON.stringify(b)); //глуюокое клонирование
+// const d = b; //ссылка
+// const e = { ...b }; //не глубокое клонирование
+
+// // не глубокое клонирование
+// const f = {};
+// Object.assign(f, b);
+
+// // глубокое клонирование - новая функция поддерживается только новыми браузерам
+// const g = structuredClone(b)
+
+// b.b.a = 42;
+// b.a = 420;
+// console.log("b", b);
+// console.log("c", c);
+// console.log("d", d);
+// console.log("e", e);
+// console.log("f", f);
+// console.log("g", g);
+
+// reduce
+const obj = {
+	A1: 1,
+	B1: 2,
+	AA1: 3,
+	AB1: 4,
+	A2: 1,
+	B2: 2,
+	AA2: 3,
+	AB2: 4,
+	A3: 1,
+	B3: 2,
+	AA3: 3,
+	AB3: 4,
 };
+const ref = ["A", "B", "AA", "AB"];
 
-const c = JSON.parse(JSON.stringify(b)); //глуюокое клонирование
-const d = b; //ссылка
-const e = { ...b }; //не глубокое клонирование
+for (const key in obj) {
+	const n = isN(key);
+	if (n < 2) continue;
+	console.log("@", key, obj[key]);
+}
 
-// не глубокое клонирование
-const f = {};
-Object.assign(f, b);
-
-// глубокое клонирование - новая функция поддерживается только новыми браузерам
-const g = structuredClone(b)
-
-b.b.a = 42;
-b.a = 420;
-console.log("b", b);
-console.log("c", c);
-console.log("d", d);
-console.log("e", e);
-console.log("f", f);
-console.log("g", g);
+function isN(k) {
+	const n = k.slice(1);
+	if (n == +n) {
+		return +n;
+	}
+	return isN(n);
+}
