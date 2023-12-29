5cf2a6df69bb18529c74f57bc559480aea14abea
xostron <aat9044107480@gmail.com>
2023-12-29 11:01:15 +0300

    api

--- 0_node_express/Clear_MEN/src/routes/api/client/auth/signup/index.js
+++ 0_node_express/Clear_MEN/src/routes/api/client/auth/signup/index.js
@@ -1,13 +1,25 @@
 const ApiError = require('@exceptions/api-error')
+const { pwd } = require('../fn')
+const { user, create } = require('./fn')
 
+// Регистрация нового пользователя
 function signup(db) {
 	return function (req, res, next) {
 		try {
-			const o = req.body
-			console.log('@@@ SIGNUP ', o)
+			const { login, password1, password2 } = req.body
+			console.log('1@@@ SIGNUP ', login, password1, password2)
+			// проверка пароля
+			if (password1 !== password2) return next(ApiError.BadRequest('Пароли должны совпадать'))
+
+			const p = [pwd(password1), user(db, login)]
+			Promise.all(p)
+				.then(([hash, check, crt]) => {
+					console.log('@2 = ', hash, ' - ', check, ' = ', crt)
+				})
+				.catch()
 			res.json({ result: 'signup' })
 		} catch (error) {
-			next(ApiError.BadRequest(e.toString()))
+			next(ApiError.BadRequest(error.toString()))
 		}
 	}
 }
--- 0_node_express/Clear_MEN/src/routes/api/client/index.js
+++ 0_node_express/Clear_MEN/src/routes/api/client/index.js
@@ -1,9 +1,13 @@
 const version = require('@middlew/version')
 const todo = require('./todo')
 const auth = require('./auth')
+
 function client(router, db) {
+
 	router.use('/client', version())
+
 	auth(router, db)
+	
 	router.get('/client/test', (req, res, next) => {
 		const result = 'test client'
 		res.json({ result })
