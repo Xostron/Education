3a4093336af405bf539a1d654fa378aecc4a540a
Xostron <aat9044107480@gmail.com>
2023-12-30 23:11:34 +0300

    фы

--- 2_js_sandbox/JS/fetch.js
+++ 2_js_sandbox/JS/fetch.js
@@ -62,6 +62,38 @@
 // console.log("g", g);
 
 // isNun, number
+// const obj = {
+// 	A1: 1,
+// 	B1: 2,
+// 	AA1: 3,
+// 	AB1: 4,
+// 	A2: 1,
+// 	B2: 2,
+// 	AA2: 3,
+// 	AB2: 4,
+// 	A3: 1,
+// 	B3: 2,
+// 	AA3: 3,
+// 	AB3: 4,
+// };
+// const ref = ["A", "B", "AA", "AB"];
+
+// for (const key in obj) {
+// 	const n = isN(key);
+// 	if (n < 2) continue;
+// 	console.log("@", key, n, obj[key]);
+// }
+
+function isN(k) {
+	const n = k.slice(1)
+	if (+n === +n) {
+		return +n
+	}
+	return isN(n)
+}
+
+// console.log('Nan = ', NaN, typeof NaN, NaN===NaN,NaN==NaN, NaN==1)
+
 const obj = {
 	A1: 1,
 	B1: 2,
@@ -75,19 +107,51 @@ const obj = {
 	B3: 2,
 	AA3: 3,
 	AB3: 4,
-};
-const ref = ["A", "B", "AA", "AB"];
-
-for (const key in obj) {
-	const n = isN(key);
-	if (n < 2) continue;
-	console.log("@", key, n, obj[key]);
 }
 
-function isN(k) {
-	const n = k.slice(1);
-	if (n == +n) {
-		return +n;
+const rows = main(obj)
+mutation(rows)
+console.log("Result = ", rows)
+
+function main(obj) {
+	const rows = []
+	const has = []
+	const raw = Object.entries(obj)
+
+	for (const key in obj) {
+		const n = isN(key)
+		if (has.includes(n)) continue
+		const prd = raw.reduce((o, el) => {
+			console.log(el[0], el[1])
+			if (+el[1] === 4) o = { ...o, [el[0]]: el[1] }
+			return o
+		}, {})
+		console.log("@ prd= ", prd)
+		has.push(n)
+		rows.push(prd)
 	}
-	return isN(n);
+	return rows
 }
+
+function mutation(rows) {
+	rows.forEach((el, idx) => {
+		// с элементом не мутирует
+		// el = {...el, 'AB1':10}
+		// записывать в исходный массив
+		rows[idx] = { ...rows[idx], AB1: el["AB1"] * 10 }
+	})
+}
+
+// пропустить итерацию, выйти из цикла нельзя
+// [1,2,3,4,5].forEach(v=>{
+// 	if (v===3) return
+// 	console.log(v)
+// })
+
+// [1,2,3,4,5].some(v=>{
+// 	if (v===3) return true
+// 	console.log(v)
+// })
+
+const in3 = [1, 2, 3, 4, 5].every((v) => v >= 1)
+console.log(in3)
