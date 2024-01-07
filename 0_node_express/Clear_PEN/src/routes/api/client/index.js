b389076c1b669c8b710d1d7004b2e41cd804c4a2
Xostron <aat9044107480@gmail.com>
2024-01-07 21:25:36 +0300

    start PEN

--- 0_node_express/Clear_PEN/src/routes/api/client/index.js
+++ 0_node_express/Clear_PEN/src/routes/api/client/index.js
@@ -6,7 +6,7 @@ function client(router, db) {
 
 	router.use('/client', version())
 
-	auth(router, db)
+	// auth(router, db)
 	
 	router.get('/client/test', (req, res, next) => {
 		const result = 'test client'
