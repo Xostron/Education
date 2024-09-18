1829502177701bd71777c0c7d091174105e3a4e7
Xostron <aat9044107480@gmail.com>
2024-09-18 21:29:11 +0300

    class

--- 2_js_sandbox/JS/class/index.js
+++ 2_js_sandbox/JS/class/index.js
@@ -9,14 +9,30 @@ class Packed {
 	setCode(val) {
 		this.code = val
 	}
+    // Публичные аксессоры
+	// getter - вычисляемое свойство
 	get full() {
 		return this
 	}
-
+	// setter - установить свойство
 	set full(obj) {
 		for (const key in obj) {
 			this[key] = obj[key]
 		}
+	}
+    // Приватные аксессоры
+    get #secret(){
+        return this.primary
+    }
+    set #secret(val){
+        this.primary = val
+    }
+	// приватный метод
+	#calc() {
+		return this.code + "-" + new Date().getFullYear() + "-" + this._id
+	}
+	setPrimary() {
+		this.primary = this.#calc()
 	}
 }
 
@@ -28,4 +44,7 @@ console.log(o.getCode())
 console.log(o.full)
 
 o.full = { _id: 10, code: "re", name: "Ro-Mu-31" }
+console.log(o)
+
+o.setPrimary()
 console.log(o)
