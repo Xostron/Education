2a8eb4f7d2c427748949a7ca023bfd32086494ef
Xostron <aat9044107480@gmail.com>
2024-10-14 23:06:46 +0300

    promise.allSettled и прерывание then

--- 2_js_sandbox/JS/async_promise/index.js
+++ 2_js_sandbox/JS/async_promise/index.js
@@ -1,12 +1,12 @@
 // Асинхронная функция - возвращает результат через 2сек
-function fnAsync(idx) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			console.log(new Date().toISOString(), "fnAsync", idx)
-			resolve(idx)
-		}, 2000)
-	})
-}
+// function fnAsync(idx) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			console.log(new Date().toISOString(), "fnAsync", idx)
+// 			resolve(idx)
+// 		}, 2000)
+// 	})
+// }
 
 //================= Неправильная обработка множества fnAsync
 // function main2() {
@@ -54,3 +54,49 @@ function fnAsync(idx) {
 // 	.then((_) => console.log(new Date().toISOString(), "Готово!"))
 // 	.catch((err) => console.log("Error", err))
 
+const a = Promise.resolve(1)
+const c = Promise.resolve(2)
+const b = Promise.resolve(3)
+
+// Promise.all([a, b, c])
+// 	.then((r) => {
+// 		console.log("all", r)
+// 	})
+// 	.catch((err) => console.log("error:", err))
+
+// Promise.allSettled([a, b, c])
+// 	.then((r) => {
+// 		console.log("allSettled", r)
+// 	})
+// 	.catch((err) => console.log("error settled", err))
+
+function fn() {
+	return new Promise((resolve, reject) => {
+		a.then((r) => {
+			console.log(1, r)
+			if (r === 1) {
+				resolve(10)
+				// return Promise.reject({ r })
+				throw new Error()
+			}
+			console.log("after")
+			return b
+		})
+			.then((r) => {
+				console.log(2, r)
+				return c
+			})
+			.then((r) => {
+				console.log(3, r)
+				resolve(r)
+			})
+			.catch((err) => {
+				console.log(55, err)
+				reject(err)
+			})
+	})
+}
+
+fn()
+	.then((r) => console.log("finish", r))
+	.catch((err) => console.log(5, err))
