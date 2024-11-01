bf9b63aaacebe985878ab34a3f82d531bacc9c6b
Xostron <aat9044107480@gmail.com>
2024-11-02 00:01:23 +0300

    time range

---
+++ 2_js_sandbox/JS/range_data/index.js
@@ -0,0 +1,35 @@
+function checkRange(begin, end) {
+	const cur = +new Date()
+		.toLocaleTimeString("ru-RU", { hour: "numeric", minute: "numeric" })
+		.split(":")
+		.join("")
+	begin = begin ? +begin.split(":").join("") : 0
+	end = end ? +end.split(":").join("") : 0
+	console.log(cur, begin, end)
+
+	// Запрет выключить
+	if (begin === end && end === 0) return false
+	// Запрет включить
+	if (begin === end && end !== 0) return true
+	// 1 случай проверки
+	if (begin !== 0 && begin > end) {
+		// Запрет включить
+		if (cur >= begin || cur < end) return true
+		// Запрет выключить
+		return false
+	}
+	// 2 случай проверки
+	if (begin < end) {
+		// Запрет включить
+		if (cur >= begin && cur < end) return true
+		// Запрет выключить
+		return false
+	}
+	// по-умолчанию Запрет выключить
+	return false
+}
+
+const begin = "23:59"
+const end = "00:01"
+
+console.log("Запрет ", checkRange(begin, end))
